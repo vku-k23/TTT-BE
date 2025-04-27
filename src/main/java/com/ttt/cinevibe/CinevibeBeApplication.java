828eeb5ca97517bf054ce0b3762d42b4cd3234ce
vietviet08 <vietnq.23ceb@vku.udn.vn>
2025-04-27 23:45:06 +0700

    docs: ✏️ init project

---
+++ src/main/java/com/ttt/cinevibe/CinevibeBeApplication.java
@@ -0,0 +1,13 @@
+package com.ttt.cinevibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinevibeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinevibeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ttt/cinevibe/CinevibeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttt.cinevibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinevibeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
